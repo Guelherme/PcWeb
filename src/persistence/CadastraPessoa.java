962a5f046a08e7b825ed55a56ea911de8f7a2ed0
lecolevati <lecolevati@yahoo.com.br>
2018-03-26 13:29:08 -0300

    Novo código

--- src/persistence/CadastraPessoa.java
+++ src/persistence/CadastraPessoa.java
@@ -20,15 +20,16 @@ public class CadastraPessoa implements ICadastraPessoa {
 	@Override
 	public void cadastraPessoa(Pessoa pessoa, PessoaMensagens pm)
 			throws SQLException {
-		String sql = "{CALL sp_inserecpf(?,?,?,?,?)}";
+		String acao = "I";
+		String sql = "{call sp_pessoa (?,?,?,?,?)}";
 		CallableStatement cs = c.prepareCall(sql);
-		cs.setString(1, pessoa.getCpf());
-		cs.setString(2, pessoa.getNome());
-		cs.setString(3, pessoa.getTelefone());
-		cs.registerOutParameter(4, Types.VARCHAR);
+		cs.setString(1, acao);
+		int cpf = Integer.parseInt(pessoa.getCpf());
+		cs.setInt(2, cpf);
+		cs.setString(3, pessoa.getNome());
+		cs.setString(4, pessoa.getTelefone());
 		cs.registerOutParameter(5, Types.VARCHAR);
 		cs.execute();
-		pm.setMsgCpf(cs.getString(4));
 		pm.setMsgCadastro(cs.getString(5));
 		cs.close();
 	}
